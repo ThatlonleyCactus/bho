a831ef822db79313149d62a6394c54d6d297950b
Jim Darden <jdarden@dardenhome.com>
2014-02-16 12:28:59 -0800

    Continuing to work on BHO WEB import code
    I switched to a different XML format. We need to figure out how to
    parse the verses now.

--- bible-history-import.py
+++ bible-history-import.py
@@ -2,37 +2,64 @@ import pymysql
 import xml.etree.ElementTree as ET
 import sys
 import codecs
-
+import unicodedata
 conn = pymysql.connect(host="jim.dardenhome.com", # your host, usually localhost
 					 port=3306,
                      user="liam", # your username
                      passwd="", # your password
                      db="bible_history") # name of the data base
 curs = conn.cursor()
-fp = codecs.open("eng-web_usfx.xml","r", "utf-8")
+fp = open("web.osis.xml","r")
 xml=fp.read()
-#rootElem = ET.parse(fp)
 rootElem = ET.fromstring(xml)
-books = rootElem.findall("book")
-bookOrder = 0;
-for book in books:
-	bookOrder += 1;
-	if (bookOrder > 39):
+bookGroups = rootElem.findall(".//*[@type='bookGroup']")
+groupNum = 0;
+for bookGroup in bookGroups:
+	groupNum += 1;
+	if (groupNum == 2):
+		continue
+	if (groupNum == 1):
 		isOldTest = 'Y'
 	else:
 		isOldTest = 'N'
 
-	for childElem in book:
-		if (childElem.tag == "toc" and childElem.attrib["level"] == "2"):
-			currentBookName = childElem.text.strip();
-		if (childElem.tag == "c"):
-			currentChapter = childElem.attrib["id"]
-		if (childElem.tag == "p"):
-			verses = childElem.findall("v")
-			for verse in verses:
-				currentVerse = verse.attrib["id"]
-				#currentVerseText = childElem.text
-				print (currentBookName + ":" + currentChapter + ":" + currentVerse)
-				curs.execute("INSERT INTO bible_history.web_verse(book, chapter, verse, verse_text, book_order, ot) " +
-					"VALUES ('" + currentBookName + "', " + str(currentChapter) + ", " + str(currentVerse) + ", 'dummy', " + str(bookOrder) + ", '" + isOldTest + "')")
-				break
+	books = bookGroup.findall(".//*[@type='book']")
+	bookOrder = 0;
+	for book in books:
+		try:
+			currentBookName = book.find("{http://www.bibletechnologies.net/2003/OSIS/namespace}title").attrib.get("short")
+			currentChapter = book.find("{http://www.bibletechnologies.net/2003/OSIS/namespace}chapter").attrib.get("sID")
+		except:
+			print("oops")
+			continue
+
+		paragraphs = book.findall("{http://www.bibletechnologies.net/2003/OSIS/namespace}p")
+		for paragraph in paragraphs:
+			print(ET.tostring(paragraph))
+			quit()
+			# for element in paragraph:
+			# 	if element.tag == "{http://www.bibletechnologies.net/2003/OSIS/namespace}verse":
+			# 		# <verse sID="Gen.1.1" osisID="Gen.1.1" />
+			# 		# In the beginning <milestone type="x-noteStartAnchor" />God
+			# 		# <note type="translation">The Hebrew word rendered “God” is “Elohim.” After “God,” 
+			# 		# the Hebrew has the two letters “Aleph Tav” (the first and last letters of the 
+			# 		# Hebrew alphabet) as a grammatical marker.</note> created the heavens and the earth.
+			# 		# <verse eID="Gen.1.1" />
+
+			# 		# see if this is the start of the verse or end
+			# 		try:
+			# 			newVerse = element.attrib.get("sID")
+			# 		except:
+			# 			# if there is no sID then this is the eID and we can ignore it
+			# 			continue
+
+			# 		# we just want the text 
+			# 		print(str(currentBookName) + ":" + str(currentChapter) + ":" + str(currentVerse))
+
+
+	# 			currentVerse = verse.attrib["id"]
+	# 			currentVerseText = childElem.text
+	# 			print (currentBookName + ":" + currentChapter + ":" + currentVerse)
+	# 			curs.execute("INSERT INTO bible_history.web_verse(book, chapter, verse, verse_text, book_order, ot) " +
+	# 				"VALUES ('" + currentBookName + "', " + str(currentChapter) + ", " + str(currentVerse) + ", 'dummy', " + str(bookOrder) + ", '" + isOldTest + "')")
+	# 			break
